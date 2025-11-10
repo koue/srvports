437bad2ac818e6347ab37021281aeccce39eaa24
Nikola Kolev <koue@chaosophia.net>
2025-11-10 20:29:33 +0000

    mkrepo script

---
+++ mkrepo.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+REPOPATH=/var/www/htdocs/pkg.chaosophia.net
+
+for path in $(find . -type d -name pkg); do
+	cp -v ${path}/* ${REPOPATH}/
+done
+
+pkg repo ${REPOPATH}/
+cp index.html ${REPOPATH}/
